1745d55835e2d52d607622ffaa8265c3b3e2bca5
wtshex1 <wtshexx@protonmail.com>
2025-07-30 19:04:38 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        port: '',
+        pathname: '/s2/favicons/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
